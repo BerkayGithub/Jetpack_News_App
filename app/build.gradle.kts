4e8003e93dcd6bb5dd31bf869ca6200c93f37f52
BerkayGithub <berkay.danis@hotmail.com>
2025-09-16 23:37:46 +0300

    Splash Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    //Splash Api
+    implementation(libs.androidx.core.splashscreen)
 }
